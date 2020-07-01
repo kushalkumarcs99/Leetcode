fb7b17240ce916aad141ebee4b92efb17cdd623e
KUSHAL KUMAR K A <kushalkumarcs99@gmail.com>
2020-07-01 12:03:04 +0530

    Add files via upload

---
+++ Premium Questions/Sum of Digits in the Minimum Number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int sumOfDigits(vector<int>& A) {
+        int n = A.size();
+
+        int min = *min_element(A.begin(), A.end());
+
+        string str = to_string(min);
+        int n1 = str.size();
+
+        int ans = 0;
+
+        while(n1)
+        {
+            ans += min%10;
+            min = min/10;
+            n1--;
+        }
+
+        if(ans%2==0)
+            return 1;
+        else
+            return 0;
+    }
+};
